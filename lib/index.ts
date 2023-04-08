f73d9f6e1ded911d2f072e4f10a28121d7d9a675
= <halillbaydar@gmail.com>
2023-04-08 08:47:12 +0300

    export bug fixed

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,6 @@ export * from "./constants/QSchedulerExpression";
 export * from "./scheduler";
 export * from "./enums/frequency";
 export * from "./enums/job.status";
-export * from "./types/db.config";
 
 export { Scheduler4JsFrequency } from "./enums/frequency";
 export { JobStatus } from "./enums/job.status";
